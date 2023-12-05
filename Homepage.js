97520c79358b2dbd8c587672c5d560a3c3aaceac
John Irvin <johnirvingeanga@gmail.com>
2023-12-05 17:12:15 +0800

    12/5

--- Homepage.js
+++ Homepage.js
@@ -1,20 +1,237 @@
-let count = 0;
-
-document.getElementById("decrement").onclick = function(){
-    count -=1;
-    document.getElementById("counter").innerHTML = count;
-
-    if(count == 0){
-        count = 0;
-        document.getElementById("counter").innerHTML = count;
-    }
+// CDM POLO 
+let Pprice, Psize;
+document.getElementById("psmall").onclick = function(){
+    Pprice = 450;
+    document.getElementById("price-display1").innerHTML = Pprice;
+    Psize = "S";
 }
-document.getElementById("reset").onclick = function(){
-    count = 0;
-    document.getElementById("counter").innerHTML = count;
-    
+document.getElementById("pmedium").onclick = function(){
+    Pprice = 450;
+    document.getElementById("price-display1").innerHTML = Pprice;
+    Psize = "M";
 }
-document.getElementById("increment").onclick = function(){
-    count +=1;
-    document.getElementById("counter").innerHTML = count;
+document.getElementById("plarge").onclick = function(){
+    Pprice = 475;
+    document.getElementById("price-display1").innerHTML = Pprice;
+    Psize = "L";
+}
+document.getElementById("pxlarge").onclick = function(){
+    Pprice = 500;
+    document.getElementById("price-display1").innerHTML = Pprice;
+    Psize = "XL";
+}
+document.getElementById("pxxlarge").onclick = function(){
+    Pprice = 525;
+    document.getElementById("price-display1").innerHTML = Pprice;
+    Psize = "XXL";
+}
+
+let Pquanti, Pnewprice;
+document.getElementById("quanti1").onclick = function(){
+    Pquanti = document.getElementById("quanti1").value;
+    Pnewprice = Pprice * Pquanti;
+    document.getElementById("price-display1").innerHTML = Pnewprice;
+}
+document.getElementById("add-cart1").onclick = function(){
+    document.getElementById("polo-quanti").innerHTML = Pquanti;
+    document.getElementById("polo-price").innerHTML = Pnewprice;
+    document.getElementById("polo-size").innerHTML = Psize;
+}
+document.getElementById("clear1").onclick = function(){
+    document.getElementById("polo-quanti").innerHTML = "";
+    document.getElementById("polo-price").innerHTML = "";
+    document.getElementById("polo-size").innerHTML = "";
+}
+
+// CDM BLOUSE
+let Bprice, Bsize;
+document.getElementById("bsmall").onclick = function(){
+    Bprice = 450;
+    document.getElementById("price-display2").innerHTML = Bprice;
+    Bsize = "S";
+}
+document.getElementById("bmedium").onclick = function(){
+    Bprice = 450;
+    document.getElementById("price-display2").innerHTML = Bprice;
+    Bsize = "M";
+}
+document.getElementById("blarge").onclick = function(){
+    Bprice = 475;
+    document.getElementById("price-display2").innerHTML = Bprice;
+    Bsize = "L";
+}
+document.getElementById("bxlarge").onclick = function(){
+    Bprice = 500;
+    document.getElementById("price-display2").innerHTML = Bprice;
+    Bsize = "XL";
+}
+document.getElementById("bxxlarge").onclick = function(){
+    Bprice = 525;
+    document.getElementById("price-display2").innerHTML = Bprice;
+    Bsize = "XXL";
+}
+
+let Bquanti, Bnewprice;
+document.getElementById("quanti2").onclick = function(){
+    Bquanti = document.getElementById("quanti2").value;
+    Bnewprice = Bprice * Bquanti;
+    document.getElementById("price-display2").innerHTML = Bnewprice;
+}
+document.getElementById("add-cart2").onclick = function(){
+    document.getElementById("blouse-quanti").innerHTML = Bquanti;
+    document.getElementById("blouse-price").innerHTML = Bnewprice;
+    document.getElementById("blouse-size").innerHTML = Bsize;
+}
+document.getElementById("clear2").onclick = function(){
+    document.getElementById("blouse-quanti").innerHTML = "";
+    document.getElementById("blouse-price").innerHTML = "";
+    document.getElementById("blouse-size").innerHTML = "";
+}
+
+// GREEN SLACKS
+let Sprice, Ssize;
+document.getElementById("ssmall").onclick = function(){
+    Sprice = 450;
+    document.getElementById("price-display3").innerHTML = Sprice;
+    Ssize = "S";
+}
+document.getElementById("smedium").onclick = function(){
+    Sprice = 450;
+    document.getElementById("price-display3").innerHTML = Sprice;
+    Ssize = "M";
+}
+document.getElementById("slarge").onclick = function(){
+    Sprice = 475;
+    document.getElementById("price-display3").innerHTML = Sprice;
+    Ssize = "L";
+}
+document.getElementById("sxlarge").onclick = function(){
+    Sprice = 500;
+    document.getElementById("price-display3").innerHTML = Sprice;
+    Ssize = "XL";
+}
+document.getElementById("sxxlarge").onclick = function(){
+    Sprice = 525;
+    document.getElementById("price-display3").innerHTML = Sprice;
+    Ssize = "XXL";
+}
+let Squanti, Snewprice;
+document.getElementById("quanti3").onclick = function(){
+    Squanti = document.getElementById("quanti3").value;
+    Snewprice = Sprice * Squanti;
+    document.getElementById("price-display3").innerHTML = Snewprice;
+}
+document.getElementById("add-cart3").onclick = function(){
+    document.getElementById("slacks-quanti").innerHTML = Squanti;
+    document.getElementById("slacks-price").innerHTML = Snewprice;
+    document.getElementById("slacks-size").innerHTML = Ssize;
+}
+document.getElementById("clear3").onclick = function(){
+    document.getElementById("slacks-quanti").innerHTML = "";
+    document.getElementById("slacks-price").innerHTML = "";
+    document.getElementById("slacks-size").innerHTML = "";
+}
+
+// PE SHIRT
+let PSprice, PSsize;
+document.getElementById("PSsmall").onclick = function(){
+    PSprice = 350;
+    document.getElementById("price-display4").innerHTML = PSprice;
+    PSsize = "S";
+}
+document.getElementById("PSmedium").onclick = function(){
+    PSprice = 350;
+    document.getElementById("price-display4").innerHTML = PSprice;
+    PSsize = "M";
+}
+document.getElementById("PSlarge").onclick = function(){
+    PSprice = 375;
+    document.getElementById("price-display4").innerHTML = PSprice;
+    PSsize = "L";
+}
+document.getElementById("PSxlarge").onclick = function(){
+    PSprice = 400;
+    document.getElementById("price-display4").innerHTML = PSprice;
+    PSsize = "XL";
+}
+document.getElementById("PSxxlarge").onclick = function(){
+    PSprice = 425;
+    document.getElementById("price-display4").innerHTML = PSprice;
+    PSsize = "XXL";
+}
+let PSquanti, PSnewprice;
+document.getElementById("quanti4").onclick = function(){
+    PSquanti = document.getElementById("quanti4").value;
+    PSnewprice = PSprice * PSquanti;
+    document.getElementById("price-display4").innerHTML = PSnewprice;
+}
+document.getElementById("add-cart4").onclick = function(){
+    document.getElementById("PEshirt-quanti").innerHTML = PSquanti;
+    document.getElementById("PEshirt-price").innerHTML = PSnewprice;
+    document.getElementById("PEshirt-size").innerHTML = PSsize;
+}
+document.getElementById("clear4").onclick = function(){
+    document.getElementById("PEshirt-quanti").innerHTML = "";
+    document.getElementById("PEshirt-price").innerHTML = "";
+    document.getElementById("PEshirt-size").innerHTML = "";
+}
+
+// PE SHIRT
+let PPprice, PPsize;
+document.getElementById("PPsmall").onclick = function(){
+    PPprice = 425;
+    document.getElementById("price-display5").innerHTML = PPprice;
+    PPsize = "S";
+}
+document.getElementById("PPmedium").onclick = function(){
+    PPprice = 425;
+    document.getElementById("price-display5").innerHTML = PPprice;
+    PPsize = "M";
+}
+document.getElementById("PPlarge").onclick = function(){
+    PPprice = 450;
+    document.getElementById("price-display5").innerHTML = PPprice;
+    PPsize = "L";
+}
+document.getElementById("PPxlarge").onclick = function(){
+    PPprice = 475;
+    document.getElementById("price-display5").innerHTML = PPprice;
+    PPsize = "XL";
+}
+document.getElementById("PPxxlarge").onclick = function(){
+    PPprice = 500;
+    document.getElementById("price-display5").innerHTML = PPprice;
+    PPsize = "XXL";
+}
+let PPquanti, PPnewprice;
+document.getElementById("quanti5").onclick = function(){
+    PPquanti = document.getElementById("quanti5").value;
+    PPnewprice = PPprice * PPquanti;
+    document.getElementById("price-display5").innerHTML = PPnewprice;
+}
+document.getElementById("add-cart5").onclick = function(){
+    document.getElementById("PEpants-quanti").innerHTML = PPquanti;
+    document.getElementById("PEpants-price").innerHTML = PPnewprice;
+    document.getElementById("PEpants-size").innerHTML = PPsize;
+}
+document.getElementById("clear5").onclick = function(){
+    document.getElementById("PEpants-quanti").innerHTML = "";
+    document.getElementById("PEpants-price").innerHTML = "";
+    document.getElementById("PEpants-size").innerHTML = "";
+}
+
+// ID LACE
+let IDprice = 60, IDquanti, IDnewprice;
+document.getElementById("quanti6").onclick = function(){
+    IDquanti = document.getElementById("quanti6").value;
+    IDnewprice = IDprice * IDquanti;
+    document.getElementById("price-display6").innerHTML = IDnewprice;
+}
+document.getElementById("add-cart6").onclick = function(){
+    document.getElementById("lace-quanti").innerHTML = IDquanti;
+    document.getElementById("lace-price").innerHTML = IDnewprice;
+}
+document.getElementById("clear6").onclick = function(){
+    document.getElementById("lace-quanti").innerHTML = "";
+    document.getElementById("lace-price").innerHTML = "";
 }
--- Homepage.php
+++ Homepage.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="./Homepage.css">
     <link rel="shortcut icon" href="./imgs/Login_&_Registration_Imgs/The_Colegio_de_Montalban_Seal.png" type="image/x-icon">
     <script defer src="./Homepage.js"></script>
+    <script src="https://kit.fontawesome.com/23c655eb58.js" crossorigin="anonymous"></script>
     <title>CDM Uniform Ordering System</title>
 </head>
 
@@ -29,32 +30,29 @@
                     <div class="uniform">
                         <img src="./imgs/Products/CDM polo.png" alt="">
                     </div>
-                    <form class="description">
+                    <form class="description" id="polo">
                         <h3 class="productname">CDM POLO</h3>
                         
                         <p class="size">Size:</p>
                         <div class="sizes">
-                            <button type="button">S</button>
-                            <button type="button">M</button>
-                            <button type="button">L</button>
-                            <button type="button">XL</button>
-                            <button type="button">XXL</button>
+                            <button type="button" id="psmall">S</button>
+                            <button type="button" id="pmedium">M</button>
+                            <button type="button" id="plarge">L</button>
+                            <button type="button" id="pxlarge">XL</button>
+                            <button type="button" id="pxxlarge">XXL</button>
                         </div>
 
                         <div class="Quantity">
-                            <label for="" class="quantilabel">Quantity</label>
-                           <label for="" class="counter" id="counter">0</label> 
-                           <button type="button" id="decrement">-</button>
-                           <button type="button" id="reset">reset</button>
-                           <button type="button" id="increment">+</button>
+                            <label for="">Quantity: </label>
+                            <input type="number" id="quanti1" min="0" max="5" placeholder="0">
                         </div>
                         
                         <div class="price">
                             <label for="">Price:</label>
-                            <p class="price-display"></p>
+                            <p class="price-display" id="price-display1"></p>
                         </div>
 
-                        <button type="button" class="add-cart">ADD TO CART</button>
+                        <button type="button" class="add-cart" id="add-cart1">ADD TO CART</button>
 
                     </form>
                 </div>
@@ -68,27 +66,24 @@
                         
                         <p class="size">Size:</p>
                         <div class="sizes">
-                            <button type="button">S</button>
-                            <button type="button">M</button>
-                            <button type="button">L</button>
-                            <button type="button">XL</button>
-                            <button type="button">XXL</button>
+                        <button type="button" id="bsmall">S</button>
+                        <button type="button" id="bmedium">M</button>
+                        <button type="button" id="blarge">L</button>
+                        <button type="button" id="bxlarge">XL</button>
+                        <button type="button" id="bxxlarge">XXL</button>
                         </div>
 
                         <div class="Quantity">
-                            <label for="" class="quantilabel">Quantity</label>
-                           <label for="" class="counter" id="counter">0</label> 
-                           <button type="button" id="decrement">-</button>
-                           <button type="button" id="reset">reset</button>
-                           <button type="button" id="increment">+</button>
+                            <label for="">Quantity: </label>
+                            <input type="number" min="0" max="5" id="quanti2" placeholder="0">
                         </div>
                         
                         <div class="price">
                             <label for="">Price:</label>
-                            <p class="price-display"></p>
+                            <p class="price-display" id="price-display2"></p>
                         </div>
 
-                        <button type="button" class="add-cart">ADD TO CART</button>
+                        <button type="button" class="add-cart" id="add-cart2">ADD TO CART</button>
 
                     </form>
                 </div>
@@ -102,27 +97,24 @@
                         
                         <p class="size">Size:</p>
                         <div class="sizes">
-                            <button type="button">S</button>
-                            <button type="button">M</button>
-                            <button type="button">L</button>
-                            <button type="button">XL</button>
-                            <button type="button">XXL</button>
+                        <button type="button" id="ssmall">S</button>
+                        <button type="button" id="smedium">M</button>
+                        <button type="button" id="slarge">L</button>
+                        <button type="button" id="sxlarge">XL</button>
+                        <button type="button" id="sxxlarge">XXL</button>
                         </div>
 
                         <div class="Quantity">
-                            <label for="" class="quantilabel">Quantity</label>
-                           <label for="" class="counter" id="counter">0</label> 
-                           <button type="button" id="decrement">-</button>
-                           <button type="button" id="reset">reset</button>
-                           <button type="button" id="increment">+</button>
+                            <label for="">Quantity: </label>
+                            <input type="number" min="0" max="5" id="quanti3" placeholder="0">
                         </div>
                         
                         <div class="price">
                             <label for="">Price:</label>
-                            <p class="price-display"></p>
+                            <p class="price-display" id="price-display3"></p>
                         </div>
 
-                        <button type="button" class="add-cart">ADD TO CART</button>
+                        <button type="button" class="add-cart" id="add-cart3">ADD TO CART</button>
 
                     </form>
                 </div>
@@ -136,27 +128,24 @@
                         
                         <p class="size">Size:</p>
                         <div class="sizes">
-                            <button type="button">S</button>
-                            <button type="button">M</button>
-                            <button type="button">L</button>
-                            <button type="button">XL</button>
-                            <button type="button">XXL</button>
+                        <button type="button" id="PSsmall">S</button>
+                        <button type="button" id="PSmedium">M</button>
+                        <button type="button" id="PSlarge">L</button>
+                        <button type="button" id="PSxlarge">XL</button>
+                        <button type="button" id="PSxxlarge">XXL</button>
                         </div>
 
                         <div class="Quantity">
-                            <label for="" class="quantilabel">Quantity</label>
-                           <label for="" class="counter" id="counter">0</label> 
-                           <button type="button" id="decrement">-</button>
-                           <button type="button" id="reset">reset</button>
-                           <button type="button" id="increment">+</button>
+                            <label for="">Quantity: </label>
+                            <input type="number" min="0" max="5" id="quanti4" placeholder="0">
                         </div>
                         
                         <div class="price">
                             <label for="">Price:</label>
-                            <p class="price-display"></p>
+                            <p class="price-display" id="price-display4"></p>
                         </div>
 
-                        <button type="button" class="add-cart">ADD TO CART</button>
+                        <button type="button" class="add-cart" id="add-cart4">ADD TO CART</button>
 
                     </form>
                 </div>
@@ -170,27 +159,24 @@
                         
                         <p class="size">Size:</p>
                         <div class="sizes">
-                            <button type="button">S</button>
-                            <button type="button">M</button>
-                            <button type="button">L</button>
-                            <button type="button">XL</button>
-                            <button type="button">XXL</button>
+                        <button type="button" id="PPsmall">S</button>
+                        <button type="button" id="PPmedium">M</button>
+                        <button type="button" id="PPlarge">L</button>
+                        <button type="button" id="PPxlarge">XL</button>
+                        <button type="button" id="PPxxlarge">XXL</button>
                         </div>
 
                         <div class="Quantity">
-                            <label for="" class="quantilabel">Quantity</label>
-                           <label for="" class="counter" id="counter">0</label> 
-                           <button type="button" id="decrement">-</button>
-                           <button type="button" id="reset">reset</button>
-                           <button type="button" id="increment">+</button>
+                            <label for="">Quantity: </label>
+                            <input type="number" min="0" max="5" id="quanti5" placeholder="0">
                         </div>
                         
                         <div class="price">
                             <label for="">Price:</label>
-                            <p class="price-display"></p>
+                            <p class="price-display" id="price-display5"></p>
                         </div>
 
-                        <button type="button" class="add-cart">ADD TO CART</button>
+                        <button type="button" class="add-cart" id="add-cart5">ADD TO CART</button>
 
                     </form>
                 </div>
@@ -202,20 +188,17 @@
                     <form class="description">
                         <h3 class="productname">CDM I.D LACE</h3>
                         
-                        <div class="Quantity">
-                            <label for="" class="quantilabel">Quantity</label>
-                           <label for="" class="counter" id="counter">0</label> 
-                           <button type="button" id="decrement">-</button>
-                           <button type="button" id="reset">reset</button>
-                           <button type="button" id="increment">+</button>
+                        <div class="Quantity adjust">
+                            <label for="">Quantity: </label>
+                            <input type="number" min="0" max="5" id="quanti6" placeholder="0">
                         </div>
                         
                         <div class="price">
                             <label for="">Price:</label>
-                            <p class="price-display"></p>
+                            <p class="price-display" id="price-display6"></p>
                         </div>
 
-                        <button type="button" class="add-cart">ADD TO CART</button>
+                        <button type="button" class="add-cart" id="add-cart6">ADD TO CART</button>
 
                     </form>
                 </div>
@@ -236,58 +219,57 @@
             <form action="" class="subsection2">
                 <h2 id="cart">CART</h2>
 
-                <div class="cart-label">
-                    <section class="mycart-products-label">
-                        <label for="">Product</label>
-                        <div class="quanti-h">Quantity</div>
-                        <div class="item-price-h">Price</div>
-                        <div class="x-button-h">Remove</div>
-                    </section>
-                </div>
-
-                <div class="mycart">
-                    <section class="mycart-products">
-                        <label for="">CDM Polo</label>
-                        <div class="quanti"></div>
-                        <div class="item-price"></div>
-                        <div class="x-button"></div>
-                    </section>
-
-                    <section class="mycart-products">
-                        <label for="">CDM Blouse</label>
-                        <div class="quanti"></div>
-                        <div class="item-price"></div>
-                        <div class="x-button"></div>
-                    </section>
-
-                    <section class="mycart-products">
-                        <label for="">Green Slacks</label>
-                        <div class="quanti"></div>
-                        <div class="item-price"></div>
-                        <div class="x-button"></div>
-                    </section>
-
-                    <section class="mycart-products">
-                        <label for="">P.E Shirt</label>
-                        <div class="quanti"></div>
-                        <div class="item-price"></div>
-                        <div class="x-button"></div>
-                    </section>
-
-                    <section class="mycart-products">
-                        <label for="">P.E Pants</label>
-                        <div class="quanti"></div>
-                        <div class="item-price"></div>
-                        <div class="x-button"></div>
-                    </section>
-
-                    <section class="mycart-products">
-                        <label for="">I.D Lace</label>
-                        <div class="quanti"></div>
-                        <div class="item-price"></div>
-                        <div class="x-button"></div>
-                    </section>
-                </div>
+                <table>
+                    <tr class="table-label">
+                        <td>Item</td>
+                        <td>Quantity</td>
+                        <td>Size</td>
+                        <td>Price</td>
+                        <td>Clear</td>
+                    </tr>
+                    <tr class="polo">
+                        <td>Polo</td>
+                        <td id="polo-quanti"></td>
+                        <td id="polo-size"></td>
+                        <td id="polo-price"></td>
+                        <td><button type="button" id="clear1"><i class="fa-solid fa-trash"></i></button></td>
+                    </tr>
+                    <tr class="blouse">
+                        <td>Blouse</td>
+                        <td id="blouse-quanti"></td>
+                        <td id="blouse-size"></td>
+                        <td id="blouse-price"></td>
+                        <td><button type="button" id="clear2"><i class="fa-solid fa-trash"></i></button></td>
+                    </tr>
+                    <tr class="slacks">
+                        <td>Slacks</td>
+                        <td id="slacks-quanti"></td>
+                        <td id="slacks-size"></td>
+                        <td id="slacks-price"></td>
+                        <td><button type="button" id="clear3"><i class="fa-solid fa-trash"></i></button></td>
+                    </tr>
+                    <tr class="PEshirt">
+                        <td>P.E Shirt</td>
+                        <td id="PEshirt-quanti"></td>
+                        <td id="PEshirt-size"></td>
+                        <td id="PEshirt-price"></td>
+                        <td><button type="button" id="clear4"><i class="fa-solid fa-trash"></i></button></td>
+                    </tr>
+                    <tr class="PEpants">
+                        <td>P.E Pants</td>
+                        <td id="PEpants-quanti"></td>
+                        <td id="PEpants-size"></td>
+                        <td id="PEpants-price"></td>
+                        <td><button type="button" id="clear5"><i class="fa-solid fa-trash"></i></button></td>
+                    </tr>
+                    <tr class="IDlace">
+                        <td>ID Lace</td>
+                        <td id="lace-quanti"></td>
+                        <td></td>
+                        <td id="lace-price"></td>
+                        <td><button type="button" id="clear6"><i class="fa-solid fa-trash"></i></button></td>
+                    </tr>
+                </table>
                 <button type="submit" class="buy-button">BUY</button>
             </form>
 
